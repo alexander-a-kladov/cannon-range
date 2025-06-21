cbfb9a05f790973ddd58940a8cbac88b72ec23c4
Alexander Kladov <alexander.a.kladov@gmail.com>
2025-06-21 21:11:09 +0400

    Add sounds and textures

--- cannon_range.py
+++ cannon_range.py
@@ -39,13 +39,105 @@ COLOR_AV = (0.5, 0, 0.5)
 COLOR_EXPLOSION = (1, 0.5, 0)
 COLOR_DIRT = (0.4, 0.3, 0.1)
 
+# Objects
+OBJ_TANK = None
+OBJ_AV = None
+
+# Fonts
 FONT = None
+
+# Sounds
 CANNON_SOUND = None
 EXPLOSION_SOUND = None
 
 # Utility clamp function
 def clamp(x, minimum, maximum):
     return max(minimum, min(x, maximum))
+
+# --- OBJ and MTL Loader ---
+def MTL(filename):
+    contents = {}
+    mtl = None
+    for line in open(filename, "r"):
+        if line.startswith('#'): continue
+        values = line.strip().split()
+        if not values: continue
+        if values[0] == 'newmtl':
+            mtl = contents[values[1]] = {}
+        elif mtl is None:
+            raise ValueError("mtl file doesn't start with newmtl stmt")
+        elif values[0] == 'map_Kd':
+            mtl['map_Kd'] = values[1]
+            surf = pygame.image.load(mtl['map_Kd'])
+            image = pygame.image.tostring(surf, 'RGBA', 1)
+            ix, iy = surf.get_rect().size
+            texid = mtl['texture_Kd'] = glGenTextures(1)
+            print(texid, filename)
+            glBindTexture(GL_TEXTURE_2D, texid)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
+        else:
+            mtl[values[0]] = list(map(float, values[1:]))
+    return contents
+
+class OBJ:
+    def __init__(self, filename, swapyz=False):
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+        self.mtl = None
+        material = None
+        for line in open(filename, "r"):
+            if line.startswith('#'): continue
+            values = line.strip().split()
+            if not values: continue
+            if values[0] == 'v':
+                v = list(map(float, values[1:4]))
+                if swapyz: v = [v[0], v[2], v[1]]
+                self.vertices.append(v)
+            elif values[0] == 'vn':
+                v = list(map(float, values[1:4]))
+                if swapyz: v = [v[0], v[2], v[1]]
+                self.normals.append(v)
+            elif values[0] == 'vt':
+                self.texcoords.append(list(map(float, values[1:3])))
+            elif values[0] in ('usemtl', 'usemat'):
+                material = values[1]
+            elif values[0] == 'mtllib':
+                self.mtl = MTL(values[1])
+            elif values[0] == 'f':
+                face = []
+                texcoords = []
+                norms = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face.append(int(w[0]))
+                    texcoords.append(int(w[1]) if len(w) > 1 and w[1] else 0)
+                    norms.append(int(w[2]) if len(w) > 2 and w[2] else 0)
+                self.faces.append((face, norms, texcoords, material))
+        self.gl_list = glGenLists(1)
+        glNewList(self.gl_list, GL_COMPILE)
+        glEnable(GL_TEXTURE_2D)
+        glFrontFace(GL_CCW)
+        for face in self.faces:
+            vertices, normals, texture_coords, material = face
+            mtl = self.mtl[material] if self.mtl and material else None
+            if mtl and 'texture_Kd' in mtl:
+                glBindTexture(GL_TEXTURE_2D, mtl['texture_Kd'])
+            elif mtl and 'Kd' in mtl:
+                glColor(*mtl['Kd'])
+            glBegin(GL_POLYGON)
+            for i in range(len(vertices)):
+                if normals[i] > 0:
+                    glNormal3fv(self.normals[normals[i] - 1])
+                if texture_coords[i] > 0:
+                    glTexCoord2fv(self.texcoords[texture_coords[i] - 1])
+                glVertex3fv(self.vertices[vertices[i] - 1])
+            glEnd()
+        glDisable(GL_TEXTURE_2D)
+        glEndList()
 
 # Draw a simple cube at origin with given size and color
 def draw_cube(size=1.0, color=(1,1,1)):
@@ -135,13 +227,22 @@ class DirtMark:
 
 # Initialize pygame and OpenGL
 def init():
-    global FONT, CANNON_SOUND, EXPLOSION_SOUND
+    global FONT, CANNON_SOUND, EXPLOSION_SOUND, OBJ_TANK, OBJ_AV
     pygame.init()
     pygame.mixer.init()
     pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL)
     FONT = pygame.font.SysFont("Arial", 32)
     CANNON_SOUND = pygame.mixer.Sound("sounds/cannon.wav")
     EXPLOSION_SOUND = pygame.mixer.Sound("sounds/explosion.wav")
+    OBJ_TANK = OBJ("tank.obj", swapyz=True)
+    OBJ_AV = OBJ("av.obj", swapyz=True)
+    glLightfv(GL_LIGHT0, GL_POSITION, (-40, 200, 100, 0.0))
+    glLightfv(GL_LIGHT0, GL_AMBIENT, (0.5, 0.5, 0.5, 1.0))
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.8, 0.8, 0.8, 1.0))
+    glEnable(GL_LIGHT0)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_COLOR_MATERIAL)
+    glShadeModel(GL_SMOOTH)
     glEnable(GL_DEPTH_TEST)
     glClearColor(0.5, 0.8, 1.0, 1)  # Sky blue
     glEnable(GL_BLEND)
@@ -167,11 +268,21 @@ def create_targets():
 
 # Draw a tank (simple cube)
 def draw_tank():
-    draw_cube(2.0, COLOR_TANK)
+    glPushMatrix()
+    glRotatef(-90,1,0,0)
+    glRotatef(90,0,0,1)
+    glScalef(0.3,0.3,0.3)
+    glCallList(OBJ_TANK.gl_list)
+    glPopMatrix()
 
 # Draw an AV (simple smaller cube)
 def draw_av():
-    draw_cube(1.2, COLOR_AV)
+    glPushMatrix()
+    glRotatef(-90,1,0,0)
+    glRotatef(90,0,0,1)
+    glScalef(0.2,0.2,0.2)
+    glCallList(OBJ_AV.gl_list)
+    glPopMatrix()
 
 # Draw cannon base
 def draw_cannon_base():
